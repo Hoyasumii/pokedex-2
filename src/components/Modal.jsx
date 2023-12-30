68ef29eace59ac5c4b4e2126cc3bb16b820053f4
Alan Reis <alanreisanjo@gmail.com>
2023-12-30 02:18:18 -0300

    Correção no fechamento do Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,7 +8,7 @@ export default function Modal({ tabIndex="-1", id, title, children }) {
                 <div className="modal-content">
                     <div className="modal-header">
                         <h1 className="modal-title fs-5">{ title }</h1>
-                        <button type="button" className="btn-close" data-bs-dismiss={ id }aria-label="Close"></button>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body text-center">
                         { children }
